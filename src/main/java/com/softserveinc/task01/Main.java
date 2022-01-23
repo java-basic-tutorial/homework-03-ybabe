549849a4a9ff1e629a44554094bbed508aa2d73b
ybabe <ybabe@softserveinc.com>
2022-01-23 19:01:45 +0200

    1 task

--- src/main/java/com/softserveinc/task01/Main.java
+++ src/main/java/com/softserveinc/task01/Main.java
@@ -15,12 +15,11 @@ package com.softserveinc.task01;
 public class Main {
     public static void main(String[] args) {
         // TODO: Uncomment code bellow after the class Human have been created
-//        Human vajsa = new Human("Vasilij", "Pjpkin", "01/01/1987", "+38 (012) 123-45-67");
-//        System.out.println("vajsa = " + vajsa);
-//        vajsa.setPhone("+38 (032) 321-45-98");
-//        System.out.println("vajsa = " + vajsa);
-//
-//        Human masha = new Human("Masha", "Rasputina", "01/12/1986", "+38 (021) 123-45-67");
-//        System.out.println("masha = " + masha);
+     Human vajsa = new Human("Vasilij", "Pjpkin", "01/01/1987", "+38 (012) 123-45-67");
+      System.out.println("vajsa = " + vajsa);
+       vajsa.setPhone("+38 (032) 321-45-98");
+       System.out.println("vajsa = " + vajsa);
+      Human masha = new Human("Masha", "Rasputina", "01/12/1986", "+38 (021) 123-45-67");
+       System.out.println("masha = " + masha);
     }
 }
