f5510e1c62ca6dbc59428137a27c34b322e8f388
ybabe <ybabe@softserveinc.com>
2022-01-23 19:25:02 +0200

    2 task

--- src/main/java/com/softserveinc/task02/City.java
+++ src/main/java/com/softserveinc/task02/City.java
@@ -1,4 +1,56 @@
 package com.softserveinc.task02;
 
 public class City {
+  String name;
+  String region;
+  String country;
+  int inhabitants;
+
+  public City() {
+  }
+  public City(String name, String region, String country, int inhabitants) {
+      this.name = name;
+      this.region = region;
+      this.country = country;
+      this.inhabitants = inhabitants;
+  }
+    public void setName (String name) {
+      this.name = name;
+  }
+    public String getName() {
+        return name;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+    public String getRegion () {
+      return region;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setInhabitants(int inhabitants) {
+        this.inhabitants = inhabitants;
+    }
+
+    public int getInhabitants() {
+        return inhabitants;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", country='" + country + '\'' +
+                ", inhabitants=" + inhabitants +
+                '}';
+    }
 }
